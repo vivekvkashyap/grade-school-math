e61bc14fc126a235914866dafb190e0deaecab4b
Vivek V Kashyap <vivekvkashyap10@gmail.com>
2021-11-20 14:57:21 +0530

    Update train.py

--- grade_school_math/train.py
+++ grade_school_math/train.py
@@ -5,6 +5,7 @@ from transformers import GPT2Config, AdamW
 from transformers import get_scheduler
 from tqdm.auto import tqdm
 from torch.utils.data import DataLoader
+import pandas as pd
 
 
 def main():
